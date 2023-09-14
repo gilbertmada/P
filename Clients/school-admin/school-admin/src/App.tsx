1e4cf413e4b5006bb31d481caf9d86c14eae975d
gilbertmada <gilbertinemilien@gmail.com>
2023-09-14 15:22:50 +0300

    first commit

---
+++ Clients/school-admin/school-admin/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Login } from './login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Login />
+    </div>
+  );
+}
+
+export default App;
